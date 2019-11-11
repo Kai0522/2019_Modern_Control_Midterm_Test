9f56f77c73a1c2d4db0dad1522a62a1a5b77fc23
Kai0522 <47813497+Kai0522@users.noreply.github.com>
2019-11-11 12:43:16 +0800

    Update MIDTERM_TEST_graduate2019.m

--- MIDTERM_TEST_graduate2019.m
+++ MIDTERM_TEST_graduate2019.m
@@ -5,7 +5,7 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 clc;clear; close all;
 
-%% Parameter table
+%%% Parameter table
 Vi=12;
 Vo=36;
 R=??????;
@@ -20,13 +20,13 @@ Cb=??????;
 Cm=??????;
 
 
-%% Electrical circuit simulation result.%%
+%%% Electrical circuit simulation result.
 Vcb=Vo-Vi;
 Vco=Vo;
 I_Li=3;
 I_Lo=1;
 
-%% Parameter of transfer function%%
+%%% Parameter of transfer function.
 Alpha=(Cm*(10^6)-Cb*(10^6))/(2*Cm*(10^6)*Cb*(10^6));
 Beta=T/(Cm*(10^6)*2);
 
@@ -39,7 +39,7 @@ b2 = (Vcb +2*Alpha*D_p*I_Lo +2*Beta*D_p*I_Li ) *Li*Cb;
 b1 = Vcb*Cb*Beta*(D_p^3-D_p^2)  -(Alpha*I_Lo +Beta*I_Li)*Beta*Cb*D_p^4+(I_Li -I_Lo)*(1+D_p)*Li;
 b0 = (I_Li -I_Lo)*Beta*D_p^4 -Vcb*D_p -Alpha*D_p^3*I_Lo -Beta*D_p^3*I_Li-3*Alpha*D_p^4*I_Lo -3*Beta*D_p^4*I_Li;
 
-%% Plot the Bode plot and try to design phase lead compensator%%
+%%% Plot the Bode plot and try to design phase lead compensator
 %%% Notice:Please plot the (1) bode plot of the compensator, (2) system bode plot,
 %%% and (3) the system bode plot after compensated.
 ......
